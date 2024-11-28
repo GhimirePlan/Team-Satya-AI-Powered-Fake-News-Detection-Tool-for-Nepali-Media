08a3413b71f02771cd74a50751ebfb60c42dd1c8
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 11:18:25 +0545

    Setup basic ui model for content_js

--- fake-news-detection-frontend/src/Extension/Main.tsx
+++ fake-news-detection-frontend/src/Extension/Main.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import './styles.css'
 export default function Main() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const MainSettings = {
+        onreportAdd: () => {
+        },
+        onissueAdd: () => {
+        },
+        onresultRequested: () => {
+        },
+        onresultResponse: () => {
+        }
+    }
+    return (
+        <>
+            <div>
+                chromeExtension
+            </div>
+            <ResultFrame MainSettings={MainSettings} />
+            <ReportFrame MainSettings={MainSettings} />
+            <FeedbackFrame MainSettings={MainSettings} />
+        </>
+
+    )
+}
+function ResultFrame({ MainSettings }) {
+    return (
+        <dialog></dialog>
+    )
+}
+function ReportFrame({ MainSettings }) {
+    return (
+        <dialog></dialog>
+    )
+}
+function FeedbackFrame({ MainSettings }) {
+    return (
+        <dialog></dialog>
+    )
 }
