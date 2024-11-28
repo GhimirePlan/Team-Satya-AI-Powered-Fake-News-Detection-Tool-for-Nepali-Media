be87ba8dca13c52a0b974e2375408e0b88964e02
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 10:13:58 +0545

    Completion of creation of Register

--- FakeNewsDetectionBackend/accounts/routes.py
+++ FakeNewsDetectionBackend/accounts/routes.py
@@ -1,5 +1,6 @@
-from .views import Login
+from .views import Login,Register
 from django.urls import path
 urlpatterns = [
-    path("login", Login,name='login')
+    path("login", Login,name='login'),
+    path("register", Register,name='login'),
 ]
--- FakeNewsDetectionBackend/accounts/views.py
+++ FakeNewsDetectionBackend/accounts/views.py
@@ -20,6 +20,39 @@ def Login(request):
                 error="Invalid Credentials"
 
     return render(request,"login.html",{"error":error})
-
+def Register(request):
+    errors={
+        "username":"",
+        "first_name":"",
+        "last_name":"",
+        "email":"",
+        "password":"",
+        "password_conf":"",
+    }
+    if request.method=="POST":
+        username=request.POST["username"]
+        email=request.POST["email"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["username"]
+        password=request.POST["password"]
+        password_conf=request.POST["password_conf"]
+        user=User(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
+        if User.objects.filter(username=username).exists():
+            errors['username']='Username already exists.'
+        else:
+            if User.objects.filter(email=email).exists():
+                errors['email']="Email already exists"
+            else:
+                try:
+                    validate_password(password,user)
+                    if password==password_conf:
+                        user.save()
+                        login(request,user)
+                        return redirect("/")
+                    else:
+                        errors['password_conf']="Both password must match"
+                except Exception as e:
+                    errors['password']=str(e)
+    return render(request,'register.html', errors)
 
         
