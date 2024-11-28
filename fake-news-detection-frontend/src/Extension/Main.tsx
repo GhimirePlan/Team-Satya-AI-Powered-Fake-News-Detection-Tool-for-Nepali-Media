a840a19ec87e21b39d569e72afd76659c04ba80b
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 14:46:09 +0545

    adding html for side button for web in content script

--- fake-news-detection-frontend/src/Extension/Main.tsx
+++ fake-news-detection-frontend/src/Extension/Main.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 export default function Main() {
+    //reference to side buton for content js
+    const MainUiButton=useRef(document.createElement("div"))
+    //creating reference to the menuitems for side buttons
+    const referenceMenuDialog = useRef(document.createElement('dialog'))
     const MainSettings = {
         onreportAdd: () => {
         },
@@ -14,7 +18,20 @@ export default function Main() {
     return (
         <>
             <div>
-                chromeExtension
+                <div className="inner" ref={MainUiButton}>
+                    <img src="https://kalika37.github.io/ReactBounceBallAndTodo/content-icon.jpg" alt="" />
+                </div >
+                <dialog className='mainMenuFrame menu' ref={referenceMenuDialog}>
+                    <div className="texter">
+                        <button>Check Selected</button>
+                        <button>Open Detector UI</button>
+                        <button>Setting</button>
+                        <button>Send Feedback</button>
+                        <button>Report Issue</button>
+                    </div>
+                    <div className="right">
+                    </div>
+                </dialog>
             </div>
             <ResultFrame MainSettings={MainSettings} />
             <ReportFrame MainSettings={MainSettings} />
@@ -23,16 +40,19 @@ export default function Main() {
 
     )
 }
+//component to show up the result of the 
 function ResultFrame({ MainSettings }) {
     return (
         <dialog></dialog>
     )
 }
+//component to send report issues
 function ReportFrame({ MainSettings }) {
     return (
         <dialog></dialog>
     )
 }
+//component to send feedback of users
 function FeedbackFrame({ MainSettings }) {
     return (
         <dialog></dialog>
