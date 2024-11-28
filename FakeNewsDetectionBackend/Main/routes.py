7ef2e2d4ed7aa7cb09bbf0696f6c820ae4017905
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 10:35:24 +0545

    creating routes for the views

--- FakeNewsDetectionBackend/Main/routes.py
+++ FakeNewsDetectionBackend/Main/routes.py
@@ -1,6 +1,10 @@
-from .views import MainPage
+from .views import MainPage,ReportIssue,FeedbackPage,ResultForExtension,ResultPage
 from django.urls import path
 
 urlpatterns = [
     path("",MainPage,name="index"),
+    path("report_issue",ReportIssue,name="issue"),
+    path("send_feedback",FeedbackPage,name="feedback"),
+    path("search_result",ResultPage,name="search"),
+    path("search_for_extension", ResultForExtension,name="e_result")
 ]
