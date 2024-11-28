be8bfc1a47dd07af05ff101300de5fdb02c2408b
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 09:41:45 +0545

    Creating models and register in admin panel

--- FakeNewsDetectionBackend/Main/admin.py
+++ FakeNewsDetectionBackend/Main/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Feedback,ReportIssue
 # Register your models here.
+admin.site.register([Feedback,ReportIssue])
--- FakeNewsDetectionBackend/Main/models.py
+++ FakeNewsDetectionBackend/Main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+class Feedback(models.Model):
+    user = models.ForeignKey(User,related_name="feedbacks", on_delete=models.CASCADE)
+    message=models.TextField()
+class ReportIssue(models.Model):
+    user = models.ForeignKey(User,related_name="issues", on_delete=models.CASCADE)
+    message=models.TextField()
+
