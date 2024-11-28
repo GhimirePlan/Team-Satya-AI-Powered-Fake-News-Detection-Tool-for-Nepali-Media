2a20ca4977e76c4193645fb78ac2af7818029027
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 10:00:46 +0545

    Completion of creation of login backend

--- FakeNewsDetectionBackend/accounts/views.py
+++ FakeNewsDetectionBackend/accounts/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.password_validation import validate_password
 # Create your views here.
 def Login(request):
-    return render(request,"login.html",{})
+    error=''
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect("/")
+            
+        else:
+            if not User.objects.filter(username=username).exists():
+                error="Account is not found.."
+            else:
+                error="Invalid Credentials"
+
+    return render(request,"login.html",{"error":error})
+
+
+        
