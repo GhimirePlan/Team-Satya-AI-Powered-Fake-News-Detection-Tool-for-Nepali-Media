fbb28054273c74d13bf26eb3e0c6340be7d8f6b3
Kalika37 <nabin.pokharel07@gmail.com>
2024-11-28 11:02:13 +0545

    Updated index.js for the checking where contentJs will run and where not

--- fake-news-detection-frontend/src/index.js
+++ fake-news-detection-frontend/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//assuring to not load extension for the domain website, other page then text/* and the chrome popup
+if (window.location.protocol !== 'chrome-extension:' && document.contentType.startsWith("text/") && window.location.host !== 'localhost:8000') {
+  const FakeNewsDetectorMainMenuButton=document.createElement('div')
+  FakeNewsDetectorMainMenuButton.id='FakeNewsDetectorMainMenuButton'
+  document.body.append(FakeNewsDetectorMainMenuButton)
+  const root = ReactDOM.createRoot(FakeNewsDetectorMainMenuButton);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
